0b38f6dc284ab6c1712e09f1029ed6b0365da645
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-09-03 00:19:35 +0100

    commit

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -21,7 +21,7 @@ def welcomeh():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def funC(text="is cool"):
+def funC(text):
     """ C followed by the value of the text variable"""
     return 'C {}'.format(text.replace('_', ' '))
 
