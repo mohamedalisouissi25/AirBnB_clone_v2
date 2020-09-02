23f9dee72db35cf4bc4e0bf42ecc7838bcbf45b2
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-09-02 12:17:30 +0100

    commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def homesh():
-    """returns Hello HBNB!"""
+    """Hello HBNB!"""
     return 'Hello HBNB!'
 
 if __name__ == "__main__":
